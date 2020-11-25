c6a4c9a5dbbc44e5a1aec138b2d28fc695f3e366
Lukas Schaller <s8lmscha@stud.uni-saarland.de>
2020-11-25 15:51:07 +0100

    Rowlayout seems to work for milestone1 build
    Still failing unit test "CHAR(4)"

--- src/RowStore.cpp
+++ src/RowStore.cpp
@@ -1,5 +1,8 @@
 #include "RowStore.hpp"
 
+uintptr_t address;
+size_t rows_used;
+
 RowStore::RowStore(const m::Table &table)
         : Store(table)
 {
@@ -7,20 +10,30 @@ RowStore::RowStore(const m::Table &table)
     std::size_t numAttributes = table.size();  //amount of attributes
     std::size_t current_offset = 0;
 
+    size_t pos = 0;
+    size_t current_align = 0;
+
+
     for(std::size_t i=0; i<table.size(); ++i){
         current_offset += table[i].type->size(); //in bits
+
+        if (table[i].type->alignment() > current_align){
+            current_align = table[i].type->alignment();
+            pos = i;
+        }
     }
+
 
 
     //round_up(sum of all attribute bits needed + bitmap bits)
     size_t row_bits_total = current_offset + numAttributes;
     size_t row_total_bytes = row_bits_total % 8 == 0 ? (row_bits_total / 8) : (row_bits_total / 8) +1;
 
-    auto bytes_first_elem = table[(size_t)0].type->size() / 8;  //changes this to element of first
+    auto bytes_first_elem = (size_t) ceil((double) table[(size_t)0].type->size()/ 8);  //changes this to element of first
     auto paddRowSize = (bytes_first_elem - ( row_total_bytes % bytes_first_elem)) % bytes_first_elem;
     std::size_t master_stride_bytes = row_total_bytes + paddRowSize;
 
-    auto address = reinterpret_cast<uintptr_t>(malloc(row_total_bytes * 100000));
+    address = (uintptr_t) malloc(row_total_bytes * 100000);
 
     /* TODO 1.2.2: Create linearization. */
     auto lin = std::make_unique<m::Linearization>(m::Linearization::CreateInfinite(1));
@@ -33,6 +46,7 @@ RowStore::RowStore(const m::Table &table)
         offset += table[i].type->size();
     }
     row->add_null_bitmap(offset, 0);
+    rows_used = 1;
 
     lin->add_sequence(address, master_stride_bytes, std::move(row));
     linearization(std::move(lin));
@@ -41,23 +55,26 @@ RowStore::RowStore(const m::Table &table)
 RowStore::~RowStore()
 {
     /* TODO 1.2.1: Free allocated memory. */
-    //free(address);
+    free((void*)address);
 }
 
 std::size_t RowStore::num_rows() const
 {
     /* TODO 1.2.1: Implement */
+    return rows_used;
 }
 
 //Append another tuple row dynamically
 void RowStore::append()
 {
     /* TODO 1.2.1: Implement */
+    rows_used++;
 }
 
 void RowStore::drop()
 {
     /* TODO 1.2.1: Implement */
+    rows_used--;
 }
 
 void RowStore::dump(std::ostream &out) const
