4c4573954b42873904b67dc0ae28e7406835efc2
Lukas <s8lmscha@stud.uni-saarland.de>
2021-01-07 15:44:48 +0100

    VERY basic implementation

--- src/MyPlanEnumerator.cpp
+++ src/MyPlanEnumerator.cpp
@@ -1,12 +1,45 @@
 #include "MyPlanEnumerator.hpp"
-
+#include <map>
 
 using namespace m;
-
+using namespace std;
 
 void MyPlanEnumerator::operator()(const QueryGraph &G, const CostFunction &CF, PlanTable &PT) const
 {
     const AdjacencyMatrix M(G); // compute the adjacency matrix for graph G
+    auto subPlans = getSubPlanBitmaps(G.sources(), M,   PT);
+    subPlans.size();
 
-    // TODO 3.1.2.2 Implement an algorithm for plan enumeration
+    for(SmallBitset s : subPlans)
+        cout << s << endl;
+
+    /*
+        SELECT T0.id, T1.id, T2.id
+        FROM T0, T1, T2
+        WHERE T0.id = T1.fid_T0 AND T1.id = T2.fid_T1;
+     */
+
+    for (auto i=0; i<subPlans.size(); i++){
+        for(auto j=1; j<subPlans.size(); j++){
+
+            if (M.is_connected(subPlans[i] | subPlans[j]))
+                PT.update(CF, subPlans[i], subPlans[j], 0);
+
+        }
+    }
+
+}
+
+
+std::vector<SmallBitset> getSubPlanBitmaps(std::vector<m::DataSource*> arr, const AdjacencyMatrix M, PlanTable &PT){
+    std::vector<SmallBitset> erg;
+
+    for(auto i=1; i< pow(2, arr.size()); i++){
+        auto tmp = SmallBitset(i);
+        //if (M.is_connected(tmp))
+            //cout << SmallBitset(i) << " : "<< PT.c(SmallBitset(i)) << endl;
+            erg.push_back(tmp);
+    }
+
+    return erg;
 }
--- src/MyPlanEnumerator.hpp
+++ src/MyPlanEnumerator.hpp
@@ -4,4 +4,8 @@
 struct MyPlanEnumerator : m::PlanEnumerator
 {
     void operator()(const m::QueryGraph &G, const m::CostFunction &CF, m::PlanTable &PT) const override;
+
+
 };
+
+std::vector<m::SmallBitset> getSubPlanBitmaps(const std::vector<m::DataSource*> arr, const m::AdjacencyMatrix M, m::PlanTable &PT);
